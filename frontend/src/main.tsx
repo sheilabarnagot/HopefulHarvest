ad73780ede871eb504191be91bab99d6d8aaf1cc
Sheila <sheila.martinezdiez@gmail.com>
2023-12-11 17:48:02 +0100

    registrationSida

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import UserRegistration from './UserRegistration.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <UserRegistration />
   </React.StrictMode>,
 )
