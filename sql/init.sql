94ef6d16fefd7b0277b523a41a6fe17ab2cffc63
Pontus A <pontus.abrahamsson@iths.se>
2023-12-12 11:24:09 +0100

    Add tables to init sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,9 +1,58 @@
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  username VARCHAR(255) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  lastname VARCHAR(255) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+-- Users Table
+CREATE TABLE Users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    address TEXT,
+    phone_number VARCHAR(20)
+);
+
+-- Categories Table
+CREATE TABLE Categories (
+    category_id SERIAL PRIMARY KEY,
+    category_name VARCHAR(50) NOT NULL,
+    description TEXT
+);
+
+-- Products Table
+CREATE TABLE Products (
+    product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    stock_quantity INTEGER NOT NULL,
+    category_id INTEGER REFERENCES Categories(category_id)
+);
+
+-- Orders Table
+CREATE TABLE Orders (
+    order_id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES Users(user_id),
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    total_amount DECIMAL(10, 2) NOT NULL,
+    order_status VARCHAR(20) NOT NULL
+);
+
+-- OrderItems Table
+CREATE TABLE OrderItems (
+    order_item_id SERIAL PRIMARY KEY,
+    order_id INTEGER REFERENCES Orders(order_id),
+    product_id INTEGER REFERENCES Products(product_id),
+    quantity INTEGER NOT NULL,
+    subtotal DECIMAL(10, 2) NOT NULL
+);
+
+-- Cart Table
+CREATE TABLE Cart (
+    cart_id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES Users(user_id)
+);
+
+-- CartItems Table
+CREATE TABLE CartItems (
+    cart_item_id SERIAL PRIMARY KEY,
+    cart_id INTEGER REFERENCES Cart(cart_id),
+    product_id INTEGER REFERENCES Products(product_id),
+    quantity INTEGER NOT NULL
 );
