78baeaad0e255a7145a2a19ae9f17fda4829b0e8
Sheila <sheila.martinezdiez@gmail.com>
2023-12-12 11:29:37 +0100

    tsconfig changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import UserRegistration from './UserRegistration.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import UserRegistration from './UserResgistration.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <UserRegistration />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
