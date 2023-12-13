6bfca5c764b96276c249733295cbc2680a8339d8
Pontus A <pontus.abrahamsson@iths.se>
2023-12-13 14:36:21 +0100

    Satisy tests

--- frontend/src/components/Modals/EditUserProfileModal.tsx
+++ frontend/src/components/Modals/EditUserProfileModal.tsx
@@ -8,10 +8,12 @@ import {
   ModalCloseButton,
   FormControl,
   FormLabel,
+  FormErrorMessage,
   Input,
   Button,
 } from '@chakra-ui/react';
-import { FormEvent, useRef } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { useEffect, useRef } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -20,12 +22,23 @@ interface EditUserProfileModalProps {
   onClose: () => void;
 }
 
+interface IFormInput {
+  email: string;
+  username: string;
+}
+
 export default function EditUserProfileModal({
   isOpen,
   onClose,
 }: EditUserProfileModalProps) {
   const initialRef = useRef(null);
-
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<IFormInput>({
+    mode: 'onChange',
+  });
   const notify = () =>
     toast('Invalid email format', {
       position: 'top-right',
@@ -38,44 +51,92 @@ export default function EditUserProfileModal({
       progress: undefined,
       theme: 'light',
     });
-  const handelSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const onSubmit: SubmitHandler<IFormInput> = async (values: IFormInput) => {
+    console.log(values);
+    console.log(errors);
+    // const respons = await fetch('http://localhost:8080/api/auth/signin', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     username: values.username,
+    //     password: values.email,
+    //   }),
+    // });
     notify();
-    console.log(e);
   };
+
+  useEffect(() => {
+    console.log(errors);
+    if (errors.email?.message === 'Invalid email format') {
+      notify();
+    }
+  }, [errors.email?.message]);
+
   return (
     <>
       <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Update account information</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            <form onSubmit={handelSubmit} id="edit-user-profile-form">
-              <FormControl>
-                <FormLabel>First name</FormLabel>
+        <form onSubmit={handleSubmit(onSubmit)} id="edit-user-profile-form">
+          <ModalContent>
+            <ModalHeader>Update account information</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormControl isInvalid={!!errors.username}>
+                <FormLabel>Update username</FormLabel>
+                <Input
+                  id="edit-user-usernamel"
+                  placeholder="username"
+                  {...register('username', {
+                    required: 'This is required',
+                    minLength: {
+                      value: 2,
+                      message: 'Minimum length should be 2',
+                    },
+                  })}
+                />
+                <FormErrorMessage id="edit-user-username-error">
+                  {errors.username && errors.username.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={!!errors.email} mt={4}>
+                <FormLabel>Update email</FormLabel>
                 <Input
                   id="edit-user-email"
-                  ref={initialRef}
-                  placeholder="Email"
+                  placeholder="email"
+                  {...register('email', {
+                    required: 'This is required',
+                    minLength: {
+                      value: 4,
+                      message: 'Minimum length should be 4',
+                    },
+                    pattern: {
+                      value: /\S+@\S+\.\S+/,
+                      message: 'Invalid email format',
+                    },
+                  })}
                 />
+                <FormErrorMessage id="edit-user-email-error">
+                  {errors.email && errors.email.message}
+                </FormErrorMessage>
               </FormControl>
-              <FormControl mt={4}>
-                <FormLabel>Last name</FormLabel>
-                <Input id="edit-user-username" placeholder="Last name" />
-              </FormControl>
-            </form>
-          </ModalBody>
+            </ModalBody>
 
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3}>
-              Save
-            </Button>
-            <Button id="edit-user-modal-close" onClick={onClose}>
-              Cancel
-            </Button>
-          </ModalFooter>
-        </ModalContent>
+            <ModalFooter>
+              <Button
+                isLoading={isSubmitting}
+                type="submit"
+                colorScheme="blue"
+                mr={3}>
+                Save
+              </Button>
+              <Button id="edit-user-modal-close" onClick={onClose}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </form>
       </Modal>
       <ToastContainer />
     </>
