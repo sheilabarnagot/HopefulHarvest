944d983d73697f4db84c69cb9ef742c1be0ea9ad
Sheila <sheila.martinezdiez@gmail.com>
2023-12-13 10:15:28 +0100

    navbar och main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,10 +5,12 @@ import App from './App.tsx'
 import './index.css'
 import UserRegistration from './UserRegistration.tsx'
 import Login from './login.tsx'
+import Navbar from './Navbar.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/register" element={<UserRegistration />} />
         <Route path="/login" element={<Login/>} />
