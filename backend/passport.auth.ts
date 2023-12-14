8ac4032f8fe5d2417a669455f102375d60c5de89
Sheila <sheila.martinezdiez@gmail.com>
2023-12-14 15:57:43 +0100

    Start with logout

--- backend/passport.auth.ts
+++ backend/passport.auth.ts
@@ -3,6 +3,10 @@ import { PassportStatic } from 'passport';
 import { users } from './auth';
 import { client } from './server';
 
+//I have delete user from line 19 after null, it was like this: if (user) {
+//   return done(null, user);
+// }
+
 const opts = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.JWT_SECRET,
@@ -11,10 +15,10 @@ const opts = {
 export const pass = (passport: PassportStatic) => {
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
-      const user = users.find(u => u.username === jwt_payload.username);
+      const user = users.find((u) => u.username === jwt_payload.username);
 
       if (user) {
-        return done(null, user);
+        return done(null);
       }
 
       return done(null, false);
