736482562887aac0178bad9c3052659d506a2211
Sheila <sheila.martinezdiez@gmail.com>
2023-12-20 15:38:09 +0100

    open modal bdd test

---
+++ frontend/cypress/e2e/testSheila/footerBDD.ts
@@ -0,0 +1,47 @@
+import {
+  Then,
+  Given,
+  Before,
+  When,
+} from '@badeball/cypress-cucumber-preprocessor';
+
+Cypress.Commands.add('login', () => {
+  cy.visit('/');
+  cy.get('#username').type('hyperslap');
+});
+
+Before(() => {
+  cy.login();
+});
+
+Given('No error is present', () => {
+  cy.get('h2').should('not.include.text', 'Unexpected Application Error!');
+});
+
+When('I navigate in the application', () => {
+  cy.visit('/');
+});
+
+Given('I am on the home page', () => {
+  cy.visit('/');
+});
+
+Then(
+  'I should see links to "About", "Shop", "Blog", "Contact Us", "Terms", and "Privacy" in the footer',
+  () => {
+    cy.get('#about-link').should('exist');
+    cy.get('#shop-link').should('exist');
+    cy.get('#blog-link').should('exist');
+    cy.get('#contact-us-link').should('exist');
+    cy.get('#terms-link').should('exist');
+    cy.get('#privacy-link').should('exist');
+  }
+);
+
+Then('I should see the copyright information in the footer', () => {
+  cy.get('#copyright-info').should('exist');
+});
+
+// Given i am on ht e home page
+// when  i click och terms
+// then a modal shoud show up
